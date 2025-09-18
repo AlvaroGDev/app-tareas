5342db0dc902bd81b032e7709ca6e8b53cea7528
Alvaro <a.gutierrez.ayestarain@gmail.com>
2025-09-18 13:47:33 +0200

    Primer comit

---
+++ back/src/test/java/es/cic/_5/es/cic/_5/ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cic._5.es.cic._5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
